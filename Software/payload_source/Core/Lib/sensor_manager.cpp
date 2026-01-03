37da60b940b6c48070f8e3537215c18ce52c9e2a
Ada Selcuk <adaselcuk16@gmail.com>
2026-01-03 14:41:54 -0500

    added macros, writeEnable, and waitForWriteComplete funcs

--- Software/payload_source/Core/Lib/sensor_manager.cpp
+++ Software/payload_source/Core/Lib/sensor_manager.cpp
@@ -6,6 +6,82 @@
 
 #include "sensor_manager.hpp"
 
+// datasheet link for further ref: https://ww1.microchip.com/downloads/aemDocuments/documents/MPD/ProductDocuments/DataSheets/25LC1024-1-Mbit-SPI-Bus-Serial-EEPROM-20002064E.pdf
+
+/* constants from datasheet! */
+#define MEM_SIZE 131072UL // 128 kb x 8 (unsigned long for math)
+#define PAGE_SIZE 256 // 256 bytes per page
+#define TIMEOUT 100 // (in ms) some HAL funcs need timeout value
+
+/* spi commands from datasheet */
+#define READ_CMD  0x03
+#define WRITE_CMD 0x02
+#define WREN_CMD  0x06 // write enable
+#define WRDI_CMD  0x04 // write disable
+#define RDSR_CMD  0x05 // read status reg
+#define WRSR_CMD  0x01 // write status reg
+/*there is also page erase but can overwrite directly so not needed (?)*/ 
+
+/* need to check status reg: bits for wpen, wip, wel, bp1, and bp0 bits  */
+#define SR_WIP 0x01 // wip (write in progress)
+#define SR_WEL 0x02 // wel (write enable latch)
+/*there are bp1 and bp0 bits for block protect but not using them(?)*/
+
+/* header needed at beginning of memory to define starting addr of each block and current size of data in each block */
+#define HEADER_SIZE 64 // in bytes (would provide space for 8 blocks just in case)
+#define NUM_RECOVERY_BLOCKS 4 // recovery data field has 4 fields: launch altitude, state, mode, packet count
+#define RECOVERY_BLOCK_SIZE 16 
+#define RECOVERY_DATA_START (HEADER_SIZE)
+#define LOG_DATA_START (HEADER_SIZE + (NUM_RECOVERY_BLOCKS * RECOVERY_BLOCK_SIZE)) // log data starts after recovery data
+#define LOG_DATA_SIZE (MEM_SIZE - LOG_DATA_START) // size available for log data
+#define LOG_LINE_TERMINATOR '\r'
+
+/* indices for recovery data blocks */
+#define BLOCK_ALTITUDE 0
+#define BLOCK_STATE 1
+#define BLOCK_MODE 2
+#define BLOCK_PACKET 3
+#define BLOCK_LOG 4
+
+SPI_HandleTypeDef hspi1; // is this correct way to access hspi1 from main.c?
+
+
+void writeEnable()
+{
+	unsigned char cmd = WREN_CMD;
+	
+	HAL_GPIO_WritePin(SPI_CS_GPIO_OUT_GPIO_Port, SPI_CS_GPIO_OUT_Pin, GPIO_PIN_RESET);
+	HAL_StatusTypeDef status = HAL_SPI_Transmit(&hspi1, &cmd, 1, TIMEOUT);
+	HAL_GPIO_WritePin(SPI_CS_GPIO_OUT_GPIO_Port, SPI_CS_GPIO_OUT_Pin, GPIO_PIN_SET);
+}
+
+
+bool waitForWriteComplete()
+{
+	unsigned char status;
+	unsigned int timeout = 0;
+	unsigned char cmd = RDSR_CMD;
+	
+	while(timeout < TIMEOUT)
+	{
+		HAL_GPIO_WritePin(SPI_CS_GPIO_OUT_GPIO_Port, SPI_CS_GPIO_OUT_Pin, GPIO_PIN_RESET);
+		HAL_SPI_Transmit(&hspi1, &cmd, 1, TIMEOUT); // read the status register
+		HAL_SPI_Receive(&hspi1, &status, 1, TIMEOUT); 
+		HAL_GPIO_WritePin(SPI_CS_GPIO_OUT_GPIO_Port, SPI_CS_GPIO_OUT_Pin, GPIO_PIN_SET);
+		
+		// if write in progress bit is cleared, write is complete
+		if((status & SR_WIP) == 0)
+		{
+			return true;
+		}
+		
+		HAL_Delay(1);
+		timeout++;
+	}
+	
+	return false;
+}
+
 SensorManager::SensorManager()
 {
     /* Initialize sensors */
