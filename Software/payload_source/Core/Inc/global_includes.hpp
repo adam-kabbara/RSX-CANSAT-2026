c1099a2e428ecab34cad38511d1cc1ec9ea3bb29
lj-watson <lukewatsontoronto@gmail.com>
2026-03-09 14:36:20 -0400

    Merge stable FSW/GSW

---
+++ Software/payload_source/Core/Inc/global_includes.hpp
@@ -0,0 +1,138 @@
+/**
+  ******************************************************************************
+  * @file           : global_includes.h
+  * @author         : RSX 2025-2026
+  * @brief          : Adds general libraries and definitions for the whole project
+  ******************************************************************************
+  */
+
+#ifndef INC_GLOBAL_INCLUDES_HPP_
+#define INC_GLOBAL_INCLUDES_HPP_
+
+#include <unordered_map>
+#include <functional>
+#include <cstring>
+#include <stdarg.h>
+#include <string>
+#include <stdio.h>
+#include <stdbool.h>
+#include <cmath>
+
+#define CMD_BUFF_SIZE 128
+#define RESP_SIZE 128
+#define SEA_LEVEL_PRESSURE_HPA 1013.25
+#define SEA_LEVEL_PRESSURE_PA 101325.0
+#define WORD_SIZE 64
+#define DATA_SIZE 32
+#define SENTENCE_SIZE 128
+#define DATA_BUFF_SIZE 512
+#define TEAM_ID 1011
+#define SENSOR_SAMPLE_RATE_HZ 20
+#define MAX_LOG_FILE_SIZE_BYTES 125000
+
+enum SimModeStatus {
+    SIM_OFF = 0,
+    SIM_EN = 1,
+    SIM_ON = 2
+};
+
+enum OperatingState {
+    LAUNCH_PAD = 0,
+    ASCENT = 1,
+    APOGEE = 2,
+    DESCENT = 3,
+    PROBE_RELEASE = 4,
+	PAYLOAD_RELEASE = 5,
+    LANDED = 6,
+    IDLE = 7
+};
+
+enum OperatingMode {
+    OPMODE_FLIGHT = 0,
+    OPMODE_SIM = 1
+};
+
+enum cam_status {
+	CAM1_ON_CAM2_ON = 0,
+	CAM1_ON_CAM2_OFF = 1,
+	CAM1_OFF_CAM2_ON = 2,
+	CAM1_OFF_CAM2_OFF = 3
+};
+
+struct rpy_data {
+	int gyro_r;
+	int gyro_p;
+	int gyro_y;
+	int accel_r;
+	int accel_p;
+	int accel_y;
+};
+
+struct bar_data {
+	float pressure;
+	float altitude;
+};
+
+struct gps_data {
+	char time[DATA_SIZE];
+	float altitude;
+	float latitude;
+	float longitude;
+	int sats;
+};
+
+struct recovery_data {
+	float launch_altitude;
+	OperatingState state;
+	OperatingMode mode;
+	int packet_count;
+};
+
+inline const char* op_mode_to_string(OperatingMode mode, int full)
+{
+	if(full == 1)
+	{
+		if(mode == OPMODE_FLIGHT)
+		{
+			return "FLIGHT";
+		}
+		else
+		{
+			return "SIM";
+		}
+	}
+	else
+	{
+		if(mode == OPMODE_FLIGHT)
+		{
+			return "F";
+		}
+		else
+		{
+			return "S";
+		}
+	}
+}
+
+inline const char* op_state_to_string(OperatingState state)
+{
+	static const char* states[] = {
+		"LAUNCH_PAD",
+		"ASCENT",
+		"APOGEE",
+		"DESCENT",
+		"PROBE_RELEASE",
+		"PAYLOAD_RELEASE",
+		"LANDED",
+		"IDLE"
+	};
+
+	return states[state];
+}
+
+inline const float pressure_to_alt(const float pressure)
+{
+	return 44330.0 * (1.0 - pow(pressure / SEA_LEVEL_PRESSURE_PA, 0.1903));
+}
+
+#endif /* INC_GLOBAL_INCLUDES_HPP_ */
---
+++ Software/payload_source/Core/Lib/sensor_manager.cpp
@@ -0,0 +1,185 @@
+/*
+ * sensor_manager.cpp
+ *
+ *  Manages the sensor code
+ */
+
+#include "sensor_manager.hpp"
+
+SensorManager::SensorManager()
+{
+    /* Declare sensors */
+}
+
+int SensorManager::updateBMP()
+{
+	return BMP5_SaveConvData(&bmp_dev);
+}
+
+float SensorManager::getPressure()
+{
+	return bmp_dev.pressure;
+}
+
+float SensorManager::getTemp()
+{
+	return bmp_dev.temperature;
+}
+
+float SensorManager::getVoltage()
+{
+	return 0.0;
+}
+
+float SensorManager::getCurrent()
+{
+	return 0.0;
+}
+
+struct rpy_data SensorManager::getIMUData()
+{
+	struct rpy_data data;
+	data.gyro_r = 0.0;
+	data.gyro_p = 0.0;
+	data.gyro_y = 0.0;
+	data.accel_r = 0.0;
+	data.accel_p = 0.0;
+	data.accel_y = 0.0;
+	return data;
+}
+
+struct gps_data SensorManager::getGPSData()
+{
+	struct gps_data data;
+	data.altitude = 0.0;
+	data.latitude = 0.0;
+	data.longitude = 0.0;
+	data.sats = 0;
+	char gps_time[DATA_SIZE] = "00:00:00";
+	strcpy(data.time, gps_time);
+	return data;
+}
+
+cam_status SensorManager::getCameraStatus()
+{
+	return cam_status::CAM1_OFF_CAM2_OFF;
+}
+
+void SensorManager::setRTCTime(int h, int m, int s)
+{
+	return;
+}
+
+void SensorManager::getRTCTime(char time_str[DATA_SIZE])
+{
+	snprintf(time_str, DATA_SIZE, "%02d:%02d:%02d", 0, 0, 0);
+}
+
+void SensorManager::getGPSTime(char time_str[DATA_SIZE])
+{
+	snprintf(time_str, DATA_SIZE, "%02d:%02d:%02d", 0, 0, 0);
+}
+
+void SensorManager::writeNoseconeServo(float val)
+{
+	servo_nosecone.SetAngle(val);
+}
+
+void SensorManager::writeContainerServo(float val)
+{
+	servo_container.SetAngle(val);
+}
+
+void SensorManager::writeWingDirServo(float val)
+{
+	servo_wing_dir.SetAngle(val);
+}
+
+void SensorManager::writeWingPWMServo(float val)
+{
+	servo_wing_pwm.SetAngle(val);
+}
+
+void SensorManager::writeElevatorServo(float val)
+{
+	servo_elevator.SetAngle(val);
+}
+
+void SensorManager::writeAileronServo(float val)
+{
+	servo_aileron.SetAngle(val);
+}
+
+void SensorManager::writeEggServo(float val)
+{
+	servo_egg.SetAngle(val);
+}
+
+void SensorManager::EEPROM_updateAltitude(float alt)
+{
+	return;
+}
+
+void SensorManager::EEPROM_updateState(OperatingState state)
+{
+	return;
+}
+
+void SensorManager::EEPROM_updateMode(OperatingMode mode)
+{
+	return;
+}
+
+void SensorManager::EEPROM_updatePackets(int count)
+{
+	return;
+}
+
+bool SensorManager::EEPROM_addLogLine(char *buffer)
+{
+	return true;
+}
+
+struct recovery_data SensorManager::EEPROM_getRecoveryData()
+{
+	struct recovery_data data;
+	data.launch_altitude = 0.0;
+	data.state = OperatingState::IDLE;
+	data.mode = OperatingMode::OPMODE_FLIGHT;
+	data.packet_count = 0;
+
+	return data;
+}
+
+void SensorManager::startSensors(SerialManager &serial, I2C_HandleTypeDef *hi2c1,
+		TIM_HandleTypeDef *htim2, TIM_HandleTypeDef *htim3, TIM_HandleTypeDef *htim4)
+{
+	/* Start all sensors that need to be started
+	 * Add a delay between each start and send an
+	 * info message */
+
+	if(BMP5_Init(&bmp_dev, hi2c1, BMP5_I2C_ADDR_FIRST))
+	{
+		serial.sendErrorMsg("BMP Init failed");
+	}
+
+	if(BMP5_Start_Mode(&bmp_dev, 1, BMP5_ODR_120HZ, BMP5_OSR_X4, BMP5_OSR_X1))
+	{
+		serial.sendErrorMsg("BMP Start Mode Init Failed");
+	}
+
+	HAL_Delay(100);
+
+
+	servo_nosecone.Init(htim4, TIM_CHANNEL_2, 1000, 2000, 180);
+	servo_container.Init(htim4, TIM_CHANNEL_1, 1000, 2000, 180);
+	servo_wing_dir.Init(htim2, TIM_CHANNEL_1, 1000, 2000, 180);
+	servo_wing_pwm.Init(htim2, TIM_CHANNEL_2, 1000, 2000, 180);
+	servo_elevator.Init(htim3, TIM_CHANNEL_1, 1000, 2000, 180);
+	servo_aileron.Init(htim3, TIM_CHANNEL_2, 1000, 2000, 180);
+	servo_egg.Init(htim3, TIM_CHANNEL_3, 1000, 2000, 180);
+
+	HAL_Delay(100);
+
+	serial.sendInfoMsg("Sensor initialization complete.");
+}
